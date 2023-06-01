df53f36ed9061364e09428df46b1fd0d4767c060
Nick Alba <NickVAlba@gmail.com>
2023-06-01 19:05:36 -0400

    Change max val for max_hikers input in CreateHikeform.js

--- ghi/src/components/hikes/CreateHikeForm.js
+++ ghi/src/components/hikes/CreateHikeForm.js
@@ -152,7 +152,7 @@ function CreateHikeForm() {
                                             name="max_hikers"
                                             type="number"
                                             autoComplete="off"
-                                            min="1" max="10"
+                                            min="1" max="100"
                                             placeholder="0"
                                             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                         />
