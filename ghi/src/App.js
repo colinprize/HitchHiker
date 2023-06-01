87e8b93dc311ac425334e80f758ea6b43993a591
Colin Prise <colinprise@MacBook-Air-3.local>
2023-06-01 15:09:35 -0700

    create hike form works

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -4,7 +4,7 @@ import Nav from './components/Nav';
 import MainPage from "./screen_view/MainPage.js";
 import ErrorNotification from "./ErrorNotification";
 import LandingPage from "./screen_view/LandingPage"
-import CreateHikeForm from "./CreateHikeForm";
+import CreateHikeForm from "./components/hikes/CreateHikeForm";
 
 
 export default function App() {
