e7664b40f5f630e1957e42f47831138ef657fc8a
Jonathan Pinon <jonathanjosephpinon@gmail.com>
2023-06-01 01:18:47 +0000

    Frontend auth

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,31 +1,16 @@
 import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Nav from './Nav';
-import MainPage from "./MainPage.js";
+import Nav from './components/Nav';
+import MainPage from "./screen_view/MainPage.js";
 import ErrorNotification from "./ErrorNotification";
-import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
-import LoginForm from "./Login";
+import LandingPage from "./screen_view/LandingPage"
+
 
 export default function App() {
   return (
-    <BrowserRouter>
-     <Routes>
-      <Route path="/" element={<LoginForm />}/>
-      {/* <Nav />
-      <div className="container">
-       
-        
-      </div> */}
-      </Routes>
-    </BrowserRouter>
-    // <AuthProvider>
-    //   <BrowserRouter>
-    //     <Nav />
-    //       <div className="container">
-    //         <Routes>
-    //         </Routes>
-    //       </div>
-    //   </BrowserRouter>
-    // </AuthProvider>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/main_page" element={<MainPage />} />
+    </Routes>
   )
 }
