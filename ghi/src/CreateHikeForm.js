dee071ebb12415397a1f540059c73dd5e7661490
Colin Prise <colinprise@MacBook-Air-3.local>
2023-06-01 14:21:19 -0700

    functions

--- ghi/src/CreateHikeForm.js
+++ ghi/src/CreateHikeForm.js
@@ -4,8 +4,8 @@ import useToken from "@galvanize-inc/jwtdown-for-react";
 function CreateHikeForm() {
     const [trail_name, setTrailName] = useState("");
     const [image_url, setURL] = useState("");
-    const [date_time, setDateTime] = useState("");
-    // const [organizer_id, setOrganizerID] = useState("")
+    const [date, setDate] = useState("")
+    const [time, setTime] = useState("")
     const [hike_description, setHikeDescription] = useState("");
     const [max_hikers, setMaxHikers] = useState("");
     const { token } = useToken();
@@ -16,8 +16,7 @@ function CreateHikeForm() {
         const data = {}
         data.trail_name = trail_name;
         data.image_url = image_url;
-        data.date_time = date_time;
-        // data.organizer_id = organizer_id;
+        data.date_time = `${date} ${time}`;
         data.hike_description = hike_description;
         data.max_hikers = max_hikers;
 
@@ -37,8 +36,8 @@ function CreateHikeForm() {
         if (response.ok) {
             setTrailName("");
             setURL("");
-            setDateTime("");
-            // setOrganizerID("");
+            setDate("");
+            setTime("");
             setHikeDescription("");
             setMaxHikers("");
         }
@@ -52,8 +51,8 @@ function CreateHikeForm() {
                 <form className="bg-pine-glade shadow-md rounded px-20 pt-6 pb-8 m-4" onSubmit={handleSubmit}>
                     <div className="space-y-12">
                         <div className="border-b border-gray-900/10 pb-12">
-                            <h2 className="text-2xl font-semibold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">Create your Hike</h2>
-                            <p className="mt-3 text-m leading-8 text-gray-600">Thanks for making a hike.  Please enter the details of your hiking adventure!</p>
+                            <h2 className="text-2xl font-semibold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">Add a new Hike</h2>
+                            <p className="mt-3 text-m leading-8 text-gray-600">Thanks for posting a hike.  Please enter the details of your hiking adventure!</p>
                             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-4">
                                 <div className="sm:col-span-2">
                                     <label htmlFor="trail_name" className="block text-sm font-medium leading-6 text-gray-900">
@@ -73,32 +72,34 @@ function CreateHikeForm() {
                                         />
                                     </div>
                                 </div>
-                                <div className="sm:col-span-2">
-                                    <label htmlFor="image_url" className="block text-sm font-medium leading-6 text-gray-900">
-                                        Picture
-                                    </label>
-                                    <div className="mt-2">
-                                        <input
-                                            required
-                                            value={image_url}
-                                            onChange={e => setURL(e.target.value)}
-                                            type="text"
-                                            name="image_url"
-                                            id="image_url"
-                                            autoComplete="off"
-                                            placeholder="Picture URL"
-                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                        />
+                                <div class="col-span-full">
+                                    <label for="cover-photo" class="block text-sm font-medium leading-6 text-gray-900">Hike Photo</label>
+                                    <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                                        <div class="text-center">
+                                            <svg class="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                                                <path fill-rule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clip-rule="evenodd" />
+                                            </svg>
+                                            <div class="mt-4 flex text-sm leading-6 text-gray-600">
+                                                <label for="file-upload" class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
+                                                    <span>Upload a file</span>
+                                                    <input id="file-upload" name="file-upload" type="file" class="sr-only"
+                                                        value={image_url}
+                                                        onChange={e => setURL(e.target.value)} />
+                                                </label>
+                                                <p class="pl-1">or drag and drop</p>
+                                            </div>
+                                            <p class="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+                                        </div>
                                     </div>
                                 </div>
                                 <div className="sm:col-span-2">
-                                    <label htmlFor="date_time" className="block text-sm font-medium leading-6 text-gray-900">Date Time</label>
+                                    <label htmlFor="date" className="block text-sm font-medium leading-6 text-gray-900">Date</label>
                                     <div className="mt-2">
                                         <input
-                                            type="datetime"
-                                            id="date_time"
-                                            value={date_time}
-                                            onChange={(e) => setDateTime(e.target.value)}
+                                            type="date"
+                                            id="date"
+                                            value={date}
+                                            onChange={(e) => setDate(e.target.value)}
                                             autoComplete="off"
                                             placeholder=""
                                             required
@@ -106,21 +107,21 @@ function CreateHikeForm() {
                                         />
                                     </div>
                                 </div>
-                                {/* <div className="sm:col-span-2">
-                                    <label htmlFor="organizer_id" className="block text-sm font-medium leading-6 text-gray-900">Organizer ID</label>
+                                <div className="sm:col-span-2">
+                                    <label htmlFor="time" className="block text-sm font-medium leading-6 text-gray-900">Time</label>
                                     <div className="mt-2">
                                         <input
-                                            type="number"
-                                            id="organizer_id"
-                                            value={organizer_id}
-                                            onChange={(e) => setOrganizerID(e.target.value)}
+                                            type="time"
+                                            id="time"
+                                            value={time}
+                                            onChange={(e) => setTime(e.target.value)}
                                             autoComplete="off"
-                                            placeholder="#"
+                                            placeholder=""
                                             required
                                             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                         />
                                     </div>
-                                </div> */}
+                                </div>
                                 <div className="sm:col-span-2">
                                     <label htmlFor="hike_description" className="block text-sm font-medium leading-6 text-gray-900">
                                         Hike Description
@@ -134,7 +135,7 @@ function CreateHikeForm() {
                                             name="hike_description"
                                             id="hike_description"
                                             autoComplete="off"
-                                            placeholder="Give us the Deets. How hard? How Far?"
+                                            placeholder="Give us the deets"
                                             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                         />
                                     </div>
@@ -166,7 +167,7 @@ function CreateHikeForm() {
                             Reset
                         </button>
                         <button type="submit"
-                            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-md hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         >Submit
                         </button>
                     </div>
