f6d894fa3623d2f64129bc4dc2d2ee456e5be7a7
Kevin Ghiringhelli <keghiringhelli@gmail.com>
2023-06-02 03:08:37 +0000

    Emergency contact and Hike list view

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -5,6 +5,7 @@ import MainPage from "./screen_view/MainPage.js";
 import ErrorNotification from "./ErrorNotification";
 import LandingPage from "./screen_view/LandingPage"
 import CreateHikeForm from "./components/hikes/CreateHikeForm";
+import ListHikes from "./screen_view/Hikes";
 import CreateRideForm from "./CreateRideForm";
 import UserFormExample from "./UserFormExample";
 import image from "./images/FE0zrlrXsAcFgjl.jpg"
@@ -15,6 +16,7 @@ export default function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/main_page" element={<MainPage />} />
       <Route path="/createhike" element={<CreateHikeForm />} />
+      <Route path="/listhikes" element={<ListHikes />} />
       <Route path="/create_ride" element={<CreateRideForm />} />
     </Routes>
   )
